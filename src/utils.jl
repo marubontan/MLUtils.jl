af109f2be3f128963db54bbfb066ec34e0f222cb
marubontan <marubontan@gmail.com>
2018-06-22 17:20:46 -0300

    add doc and change some names

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,31 @@
-function OneHotEncode(data::DataFrame)
+using DataFrames
+
+"""
+    oneHotEncode(data)
+
+Do one-hot-encoding to the input data.
+
+# Argument
+- `data::DataFrame`: DataFrame from DataFrames package which contains only one-hot-encoding target
+
+# Examples
+```julia-OneHotEncode
+julia> dataA = DataFrame(x=[1, 2], y=[3, 4])
+2×2 DataFrames.DataFrame
+│ Row │ x │ y │
+├─────┼───┼───┤
+│ 1   │ 1 │ 3 │
+│ 2   │ 2 │ 4 │
+
+julia> oneHotEncode(dataA)
+2×4 DataFrames.DataFrame
+│ Row │ x_1 │ x_2 │ y_3 │ y_4 │
+├─────┼─────┼─────┼─────┼─────┤
+│ 1   │ 1   │ 0   │ 1   │ 0   │
+│ 2   │ 0   │ 1   │ 0   │ 1   │
+```
+"""
+function oneHotEncode(data::DataFrame)
 
     columns = names(data)
     rowSize = nrow(data)
@@ -6,17 +33,18 @@ function OneHotEncode(data::DataFrame)
     output = DataFrame()
     for (i,column) in enumerate(columns)
 
-        uniqueValues = sort(unique(data[column]))
+        sortedUniqueValues = sort(unique(data[column]))
 
         # make label
         columnStr = string(column)
-        label = Symbol.([columnStr * "_" * string(val) for val in uniqueValues])
+        label = Symbol.([columnStr * "_" * string(val) for val in sortedUniqueValues])
 
-        oneHotEncoded = zeros(rowSize, length(uniqueValues))
+        oneHotEncoded = zeros(Int, rowSize, length(sortedUniqueValues))
         for (row,val) in enumerate(data[column])
-            col = find(uniqueValues .== val)
 
-            oneHotEncoded[row, col] = 1
+            col = find(sortedUniqueValues .== val)
+
+            oneHotEncoded[row, col] = one(Int)
         end
         oneHotEncodedDF = DataFrame(oneHotEncoded)
 
--- test/unitTest.jl
+++ test/unitTest.jl
@@ -4,11 +4,11 @@ include("../src/utils.jl")
 
 @testset "one hot encoding" begin
     dataA = DataFrame(x=[1, 2], y=[3, 4])
-    @test OneHotEncode(dataA) == DataFrame(x_1=[1, 0], x_2=[0, 1], y_3=[1, 0], y_4=[0, 1])
+    @test oneHotEncode(dataA) == DataFrame(x_1=[1, 0], x_2=[0, 1], y_3=[1, 0], y_4=[0, 1])
 
     dataB = DataFrame(x=['a', 'b'], y=['c', 'd'])
-    @test OneHotEncode(dataB) == DataFrame(x_a=[1, 0], x_b=[0, 1], y_c=[1, 0], y_d=[0, 1])
+    @test oneHotEncode(dataB) == DataFrame(x_a=[1, 0], x_b=[0, 1], y_c=[1, 0], y_d=[0, 1])
 
     dataC = DataFrame(x=['a', 'a'])
-    @test OneHotEncode(dataC) == DataFrame(x_a=[1, 1])
+    @test oneHotEncode(dataC) == DataFrame(x_a=[1, 1])
 end
